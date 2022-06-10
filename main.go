6979f5764a2680fdf301de4ed3942f33bd5f9493
jiachen.zhang <zhangjc1999@gmail.com>
2022-06-10 22:48:28 +0800

    [Feature] drone lark plugin

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type (
+	Plugin struct {
+		Token string // token for custom lark bot
+	}
+)
+
+var (
+	version = "unknown"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "drone lark plugin"
+	app.Usage = "drone lark plugin"
+	app.Action = run
+	app.Version = version
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "token",
+			Usage:   "you can get the access token when you add a bot in a group.",
+			EnvVars: []string{"TOKEN"},
+		},
+		&cli.StringFlag{
+			Name:    "color",
+			Usage:   "color the title for easier identification.",
+			EnvVars: []string{"MESSAGE_COLOR"},
+		},
+		&cli.StringFlag{
+			Name:    "title",
+			Usage:   "message title.",
+			EnvVars: []string{"MESSAGE_TITLE"},
+		},
+		&cli.StringFlag{
+			Name:    "content",
+			Usage:   "message content.",
+			EnvVars: []string{"MESSAGE_CONTENT"},
+		},
+		&cli.StringFlag{
+			Name:    "commit.sha",
+			Usage:   "git commit sha",
+			EnvVars: []string{"DRONE_COMMIT_SHA"},
+			Value:   "00000000",
+		},
+		&cli.StringFlag{
+			Name:    "commit.ref",
+			Usage:   "git commit ref",
+			EnvVars: []string{"DRONE_COMMIT_REF"},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) (err error) {
+	template := DefaultTemplate{
+		Color:           c.String("color"),
+		Title:           c.String("title"),
+		MarkdownContent: c.String("content"),
+	}
+	content, err := template.Content()
+	if err != nil {
+		log.Fatal(err)
+	}
+	url := fmt.Sprintf("https://open.feishu.cn/open-apis/bot/v2/hook/%s", c.String("token"))
+	contentType := "application/json"
+	resp, err := http.Post(url, contentType, strings.NewReader(content))
+	if err != nil {
+		return err
+	}
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+	log.Println(bodyString)
+	return
+}
---
+++ template.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"text/template"
+)
+
+type (
+	// DefaultTemplate arguments for DEFAULT_TEMPLATE
+	DefaultTemplate struct {
+		Color           string
+		Title           string
+		MarkdownContent string
+	}
+)
+
+const (
+	// DEFAULT default template
+	DEFAULT = `
+{
+    "msg_type": "interactive",
+    "card": {
+        "config": {
+            "wide_screen_mode": true
+        },
+        "header": {
+            "template": "{{.Color}}",
+            "title": {
+                "content": "{{.Title}}",
+                "tag": "plain_text"
+            }
+        },
+        "elements": [
+            {
+                "tag": "markdown",
+                "content": "{{.MarkdownContent}}"
+            }
+        ]
+    }
+}
+`
+)
+
+func (d DefaultTemplate) Content() (string, error) {
+	var buf bytes.Buffer
+	t := template.Must(template.New("default_template").Parse(DEFAULT))
+	if err := t.Execute(&buf, d); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
